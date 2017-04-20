e70ae7b68c171033800447b9fedbce6dba7b0bd9
Parker Donat <pdonat@anonyome.com>
2017-04-19 21:36:31 -0600

    Finished roll a ball.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	public Text countText;
+	public Text winText;
 
 	private Rigidbody rb;
+	private int count;
 
 	void Start()
 	{
 		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
 	}
 
 	void FixedUpdate() 
@@ -23,4 +30,19 @@ public class PlayerController : MonoBehaviour {
 		rb.AddForce (movement * speed);
 	} 
 
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}	
+
+	void SetCountText () {
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) {
+			winText.text = "You Win!";
+		}
+	}
 }
